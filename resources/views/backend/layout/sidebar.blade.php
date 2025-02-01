e4afb45ca8c541ecc17a6ad75f922f888b41aa23
Zilani <abdulkaderzilani98@gmail.com>
2025-02-02 02:14:35 +0600

    product sidebar modification done

--- resources/views/backend/layout/sidebar.blade.php
+++ resources/views/backend/layout/sidebar.blade.php
@@ -16,27 +16,27 @@
             <li><a href="#product" aria-expanded="false" data-toggle="collapse"> <i class="dripicons-list"></i><span>{{__('file.product')}}</span><span></a>
             <ul id="product" class="collapse list-unstyled ">
                 @if($category_permission_active)
-                <li id="category-menu"><a href="{{route('category.index')}}">{{__('file.category')}}</a></li>
+                <li id="category-menu"><a href="{{route('category.index')}}"> Product Style</a></li>
                 @endif
                 @if($index_permission_active)
-                <li id="product-list-menu"><a href="{{route('products.index')}}">{{__('file.product_list')}}</a></li>
+                <li id="product-list-menu"><a href="{{route('products.index')}}">Current List</a></li>
                 <?php
                     $add_permission_active = $role_has_permissions_list->where('name', 'products-add')->first();
                 ?>
                 @if($add_permission_active)
-                <li id="product-create-menu"><a href="{{route('products.create')}}">{{__('file.add_product')}}</a></li>
+                <li id="product-create-menu"><a href="{{route('products.create')}}"> Add New Product</a></li>
                 @endif
                 @endif
-                @if($print_barcode_active)
+                <!-- @if($print_barcode_active)
                 <li id="printBarcode-menu"><a href="{{route('product.printBarcode')}}">{{__('file.print_barcode')}}</a></li>
-                @endif
-                @if($adjustment_active)
+                @endif -->
+                <!-- @if($adjustment_active)
                 <li id="adjustment-list-menu"><a href="{{route('qty_adjustment.index')}}">{{trans('file.Adjustment List')}}</a></li>
                 <li id="adjustment-create-menu"><a href="{{route('qty_adjustment.create')}}">{{trans('file.Add Adjustment')}}</a></li>
-                @endif
-                @if($stock_count_active)
+                @endif -->
+                <!-- @if($stock_count_active)
                 <li id="stock-count-menu"><a href="{{route('stock-count.index')}}">{{trans('file.Stock Count')}}</a></li>
-                @endif
+                @endif -->
             </ul>
             </li>
             @endif
